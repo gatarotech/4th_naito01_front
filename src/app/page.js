fd3e004bd4f804e33efc4c27b12bbe25d8413f38
gataro <tacoooon@gmail.com>
2025-02-01 16:32:54 +0900

    試しにプチ変更

--- src/app/page.js
+++ src/app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          がたろー
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +78,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          テスト
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
